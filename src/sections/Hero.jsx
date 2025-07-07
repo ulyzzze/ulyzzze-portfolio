be5c7f536f7ede04e15d3d52534f23a64957e4ab
ulyzzze <ulysse.couchoud@epitech.eu>
2025-07-07 17:08:35 +0200

    add: hero sections

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='hero' className='relative overflow-hidden'>
+      <div className='absolute top-0 left-0 z-10'>
+        <img src='/images/bg.png' alt="background" />
+      </div>
+
+      <div className='hero-layout'>
+        <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
