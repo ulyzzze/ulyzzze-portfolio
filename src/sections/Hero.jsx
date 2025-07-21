6e648c82c2fb246e37617dfade2f3166a25c6030
ulyzzze <ulysse.couchoud@epitech.eu>
2025-07-21 21:12:11 +0200

    fix: navbar bottom border

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
 
   return (
     <section id='hero' className='relative overflow-hidden '>
-      <div className='absolute top-0 left-0 z-10'>
+      <div className='absolute top-0 left-0 z-50'>
         <img src='/images/bg.png' alt="background" />
       </div>
         <header>
